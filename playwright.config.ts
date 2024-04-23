7c1f77ed1df9948e2240aeef7576efb1fa62bbfa
Jaz Singh <jazeee@gmail.com>
2024-04-23 15:32:20 -0700

    Add port

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,6 +71,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
+    port: 5173,
     url: 'http://127.0.0.1:5173',
     reuseExistingServer: !process.env.CI,
   },
